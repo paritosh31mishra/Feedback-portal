ef46dfb5a477f15fe5902018d4e32b05cf29d616
Paritosh Mishra <paritosh31mishra@gmail.com>
2025-01-22 19:51:48 +0530

    Update review_header.js

--- reactapp/src/review_header.js
+++ reactapp/src/review_header.js
@@ -1,15 +1,15 @@
 import {Link}  from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import {GoogleLogout} from 'react-google-login';
-import { gapi } from 'gapi-script';
+import { googleLogout } from '@react-oauth/google';
 
 const clientId = "925306536966-8krnsg2kdor08se5s6d47vla1pec5f2f.apps.googleusercontent.com";
 const Reviewheader = () =>{
 
-  const logout = () =>{
-    window.localStorage.clear();
-    window.location.href="http://localhost:3000/#/"; // it will go to dashboard
-    window.location.reload(); // it will reload the app. js(window) file
+  const logout = () => {
+    googleLogout(); // Perform Google logout
+    window.localStorage.clear(); // Clear localStorage
+    window.location.href = "https://weather-application-1-reactapp.onrender.com/#/"; // Redirect to dashboard
+    window.location.reload(); // Reload the app
   }
   
     let[isHidden, setIsHidden] = useState(true);
@@ -20,12 +20,18 @@ const Reviewheader = () =>{
        
       <div class="btn-group" role="group" >
       <Link to="/Addfeedback" className="menu"> {isHidden && <button className="btn btn-dark fw-bold" type="submit" onClick={() => setIsHidden(false)}>Add Feedback</button>} </Link>
-        <button className="btn btn-dark fw-bold" onClick={logout}> Welcome - {localStorage.getItem("fullname") } Logout <i className="fa fa-power-off"></i>  </button>
+       <button className="btn btn-dark fw-bold" onClick={logout}>
+            Welcome - {localStorage.getItem("fullname")} Logout <i className="fa fa-power-off"></i>
+          </button>
 </div>   
  </div>
  </nav>
    )
 }
 export default Reviewheader;
+
+
+
+
 
 
